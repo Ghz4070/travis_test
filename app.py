2a2c3e4471bdcd2d10976b282842943df385f000
Ilies Ghezal <ghezal-ilies@outlook.fr>
2021-04-01 15:34:28 +0200

    edit app.py and travis.yml

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print('Hello world')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello world !"
+
+
+@app.route('/hello/<name>')
+def hello_world(name):
+    return str('Your slug is : ' + name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
